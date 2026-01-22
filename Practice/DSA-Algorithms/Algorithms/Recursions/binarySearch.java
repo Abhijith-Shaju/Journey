9b3edd3d0140814a8f5b643d38d7696dfd787df1
Abhijith Shaju <abhijithshaju2004@gmail.com>
2026-01-22 10:16:41 +0530

    binary mid calc optimization

--- Practice/DSA-Algorithms/Algorithms/Recursions/binarySearch.java
+++ Practice/DSA-Algorithms/Algorithms/Recursions/binarySearch.java
@@ -1,11 +1,9 @@
 public class binarySearch {
-    
     public static int binary(int[] arr, int start, int end, int target){
-        
         if(start > end){
             return -1;
         }
-        int mid = (start + end) / 2;
+        int mid = start + (end - start) / 2;
 
         if(arr[mid] == target){
             return mid;
@@ -18,6 +16,6 @@ public class binarySearch {
     }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        System.out.println(binary(arr, 0, arr.length-1, 0));
+        System.out.println(binary(arr, 0, arr.length-1, 6));
     }
 }
